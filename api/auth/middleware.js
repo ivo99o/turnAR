343277e92f631684acdce9c3f8987d211400dbc2
Patricio Anastacio <52506847+PatoAnastacio207@users.noreply.github.com>
2026-04-13 03:21:14 -0300

    Clean up

--- api/auth/middleware.js
+++ api/auth/middleware.js
@@ -2,7 +2,7 @@ import { verify } from './jwt.js';
 
 export default async function middleware(ctx, next) {
   const header = ctx.headers.authorization;
-  console.log(header);
+
   if (!header?.startsWith('Bearer ')) return ctx.throw(401, 'No token');
   try {
     ctx.state.user = verify(header.slice(7)); // koa convention: ctx.state for user
